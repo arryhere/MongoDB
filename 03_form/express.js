117786b4f72253eaec31516e7ef12a0a6e88f9e2
Arijit <arijitdasofficial@gmail.com>
2022-07-16 17:57:49 +0530

    new-files

--- 03_form/express.js
+++ 03_form/express.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const app = express()
 const path = require('path')
 const fs = require('fs')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/form-db', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const app = express()
+// Mongoose
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('connected');
+});
+const formSchema = new mongoose.Schema({
+    _id: Number,
+    name: String,
+    age: Number,
+    gender: String,
+    address: String,
+    more: String
+})
+const formData = mongoose.model('formData', formSchema)
 
 // log req url
 app.use((req, res, next) => {
@@ -25,14 +43,25 @@ app.get('/form', (req, res) => {
     res.status(200).render('form.pug', { title: 'Gym Membership' })
 })
 
+app.get('/form/data', (req, res) => {
+    formData.find().then((formData) => {
+        res.status(200).json(formData)
+    }).catch((error) => {
+        res.status(400).json({status: error})
+    })
+})
+
 app.get('*', (req, res) => {
     res.status(200).render('error.pug', { title: 'Gym Membership' })
 })
 
 app.post('/backend', (req, res) => {
-    const { name, age, gender, address, more } = req.body
-    console.log(name, age, gender, address, more);
-    res.status(200).send('form submitted successfully')
+    const form = new formData(req.body)
+    form.save().then(() => {
+        res.status(200).json({success: true, comment: 'form submit successful', data: req.body})
+    }).catch((error) => {
+        res.status(400).json({success: false, comment: ['form submit failed', error], data: req.body})
+    })
 })
 
 
